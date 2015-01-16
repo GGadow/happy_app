50d95a05f2132659c7e0421436810d7a970aed38
GGadow <gpg@gregory-gadow.net>
2015-01-15 20:16:01 -0800

    Added quotes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116040012) do
+ActiveRecord::Schema.define(version: 20150116041432) do
 
   create_table "jokes", force: :cascade do |t|
     t.text     "setup"
     t.text     "punchline"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.text     "saying"
+    t.string   "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
