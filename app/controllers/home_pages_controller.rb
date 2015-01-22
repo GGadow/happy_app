aab76acdb3f757c96eb1cd8190ad1790671010e3
GGadow <gpg@gregory-gadow.net>
2015-01-21 23:13:17 -0800

    Save

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -5,6 +5,20 @@
 # We need a corresponding template, so add one to app/views/home_pages
 
 class HomePagesController < ApplicationController
+
   def home #matches app/views/home_pages/home.html.erb
+
   end
+
+  def create
+    respond_to do |format|
+      if params[:table] == "quotes"
+        o = Quote.offset(rand(Quote.count)).first
+      elsif params[:table] == "jokes"
+        o = Joke.offset(rand(Joke.count)).first
+      end
+      format.html { redirect_to root_path, notice: "#{o.part1} #{o.part2}" }
+    end 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :jokes
 
-  resources :home_pages 
+  resources :home_pages
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
